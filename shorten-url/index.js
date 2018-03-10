0b84f674e284ef29e348703d47d89de4d3ca7e5e
Brian Rinaldi <brian.rinaldi@gmail.com>
2018-03-09 22:24:59 -0500

    conforming to style guide

--- shorten-url/index.js
+++ shorten-url/index.js
@@ -1,52 +1,8 @@
-const   sdk = require('kinvey-flex-sdk'),
-        request = require('request'),
-        configKey = 'Google URL Shortener API';
+const sdk = require('kinvey-flex-sdk');
+const handlers = require('./lib/handlers');
 
 sdk.service((err, flex) => {
-    const   flexFunctions = flex.functions;
-    
-    function shortenURL(context, complete, modules) {
-        var requestOptions = {
-            uri: 'https://www.googleapis.com/urlshortener/v1/url?key=',
-            body: {
-                longUrl: context.body.longUrl,
-            },
-            json: true,
-            resolveWithFullResponse: true
-        };
-        _getConfig(complete, modules).then( (result) => {
-            requestOptions.uri += result;
-            request.post(requestOptions, (error, res, body) => {
-                if (error){
-                    complete().setBody(error);
-                } else {
-                    complete().setBody({"shortUrl": body.id }).done();
-                }
-            });
-        })
-    }
+  const flexFunctions = flex.functions;
 
-    function _getConfig(complete, modules) {
-        return new Promise(resolve => {
-            const   options = {
-                        useUserContext: false
-                    },
-                    store = modules.dataStore(options),
-                    config = modules.dataStore().collection("config"),
-                    query = new modules.Query();
-            
-            query.equalTo('configKey',configKey);
-            config.find(query,(err, result) => {
-                if (err) {
-                    reject(err);
-                }
-                else if (result === undefined || result[0] === undefined ||result[0]["configVal"] === undefined) {
-                    reject(new Error("Configuration value does not exist"));
-                }
-                resolve(result[0].configVal);
-            });
-        });
-    }
-
-    flexFunctions.register('shortenURL', shortenURL);
+  flexFunctions.register('shortenURL', handlers.shortenURL);
 });
---
+++ shorten-url/lib/handlers.js
@@ -0,0 +1,49 @@
+const request = require('request');
+
+const configKey = 'Google URL Shortener API';
+
+function _getConfig(complete, modules) {
+  return new Promise((resolve) => {
+    const options = {
+      useUserContext: false
+    };
+    const store = modules.dataStore(options);
+    const config = store.collection('config');
+    const query = new modules.Query();
+
+    query.equalTo('configKey', configKey);
+    config.find(query, (err, result) => {
+      if (err) {
+        reject(err);
+      } else if (result === undefined || result[0] === undefined || result[0].configVal === undefined) {
+        reject(new Error('Configuration value does not exist'));
+      }
+      resolve(result[0].configVal);
+    });
+  });
+}
+
+function shortenURL(context, complete, modules) {
+  const requestOptions = {
+    uri: 'https://www.googleapis.com/urlshortener/v1/url?key=',
+    body: {
+      longUrl: context.body.longUrl
+    },
+    json: true,
+    resolveWithFullResponse: true
+  };
+  _getConfig(complete, modules).then((result) => {
+    requestOptions.uri += result;
+    request.post(requestOptions, (error, res, body) => {
+      if (error) {
+        complete().setBody(error);
+      } else {
+        complete()
+          .setBody({ shortUrl: body.id })
+          .done();
+      }
+    });
+  });
+}
+
+exports.shortenURL = shortenURL;
